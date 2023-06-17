6e01ac27e8f1291a369f8855a541cf37ef530123
Rodo Vilcarromero <rodo.vilcarromero@utec.edu.pe>
2023-06-17 11:37:44 -0500

    Update recoveryData.py version final

--- recoveryData.py
+++ recoveryData.py
@@ -1,68 +1,74 @@
 import nltk
+from nltk.stem.snowball import SnowballStemmer
 from nltk.corpus import stopwords
 from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
 nltk.download('stopwords')
 nltk.download('punkt')
-diccio_deindices = {}
 
 
-# Desglozar la query
-def recovery_data(query):
-    result = {}
-    query = query.split()
-    stop_words = set(stopwords.words('english'))
-    stop_words.discard('no')
-    stop_words = [*stop_words]
-    query_evaluate = [x for x in query if x not in stop_words]
-    if len(query_evaluate) == 0:
-        raise Exception('this query does not require any information')
-    x = 0
-    stemmer = SnowballStemmer("english")
-    while x < (len(query_evaluate)):
-        if query_evaluate[x] != 'no':
+class Recovery:
+    # Constructor de clase
+    def __init__(self, query):
+        self.query = query
 
-            result[stemmer.stem(query_evaluate[x])] = True
-            x = x + 1
-        else:
-            result[stemmer.stem(query_evaluate[x + 1])] = False
-            x = x + 2
-    # Retorna la query desenpaquetada
-    return result
-def serach_document(query_read):
-    Yes_documents = []
-    No_documents = []
-    for key, value in query_read.items():
-        if value:
-            Yes_documents.append(dictWords[key][0])
-        else:
-            No_documents.append(dictWords[key][0])
-    result = [x for x in Yes_documents if x not in No_documents]
-    # devuelve que documentos debemos
-    return result
+    # Desglozar la query
+    def recovery_query(self):
+        result = {}
+        query = self.query.split()
+        stop_words = set(stopwords.words('english'))
+        stop_words.discard('no')
+        stop_words = [*stop_words]
+        query_evaluate = [x for x in query if x not in stop_words]
+        if len(query_evaluate) == 0:
+            raise Exception('this query does not require any information')
+        x = 0
+        stemmer = SnowballStemmer("english")
+        while x < (len(query_evaluate)):
+            if query_evaluate[x] != 'no':
 
+                result[stemmer.stem(query_evaluate[x])] = True
+                x = x + 1
+            else:
+                result[stemmer.stem(query_evaluate[x + 1])] = False
+                x = x + 2
+        # Retorna la query desenpaquetada
+        return result
 
-# Sacar Tfidf de los documentos los cuales debemos mostrar
-def sort_document(indices, query):
-    documents = []
-    print(indices)
-    print(indices)
-    for x in indices:
-        temp = dictDocs[x][1]
-        file = open('part1.json', 'rb')
-        file.seek(x + 1) if x != 0 else file.seek(x)
-        contenido = str(file.readline().decode('utf-8'))
-        documents.append(contenido)
-    print(documents)
-    model_fit = TfidfVectorizer()
-    aux = TfidfVectorizer()
-    model_fit.fit(documents)
-    tf_idf_vector = aux.fit_transform(documents)
-    tf_ = model_fit.transform([query])
-    cosine_similary = cosine_similarity(tf_, tf_idf_vector)
-    cosine_similary_final = []
-    for i in range(len(indices)):
-        cosine_similary_final.append((cosine_similary[0][i], indices[i]))
-    cosine_similary_final.sort(key=lambda x: x[0], reverse=True)
-    return cosine_similary_final
+    def serach_document(self, query_read):
+        Yes_documents = []
+        No_documents = []
+        for key, value in query_read.items():
+            if value:
+                Yes_documents.append(dictWords[key][0])
+            else:
+                No_documents.append(dictWords[key][0])
+        result = [x for x in Yes_documents if x not in No_documents]
+        # devuelve que documentos debemos
+        return result
+
+    # Sacar Tfidf de los documentos los cuales debemos mostrar
+    def sort_document(self, indices):
+        documents = []
+        for x in indices:
+            temp = dictDocs[x][1]
+            file = open('part1.json', 'rb')
+            file.seek(temp + 1) if x != 0 else file.seek(temp)
+            contenido = str(file.readline().decode('utf-8'))
+            documents.append(contenido)
+        model_fit = TfidfVectorizer()
+        tf_idf_vector = model_fit.fit_transform(documents)
+        tf_ = model_fit.transform([self.query])
+        cosine_similary = cosine_similarity(tf_, tf_idf_vector)
+        cosine_similary_final = []
+        for i in range(len(indices)):
+            cosine_similary_final.append((cosine_similary[0][i], indices[i]))
+        cosine_similary_final.sort(key=lambda x: x[0], reverse=True)
+        return cosine_similary_final
+
+    def Recovery_data(self):
+        temp = self.recovery_query()
+        aux = self.serach_document(temp)
+        result = self.sort_document(aux)
+        return result
